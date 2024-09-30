a60b284c910b25b01d9d0ef7e41228539967e769
desaaraa <saritacatalanbaron63@gmail.com>
2024-09-30 08:33:56 +0200

    actualizadoo

--- misProyectos/src/misProyectos/holaMundo.java
+++ misProyectos/src/misProyectos/holaMundo.java
@@ -4,8 +4,13 @@ public class holaMundo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola Mundo");
-
+	String myArray[];  // para crear un array definiremos de que tipo, es decir,
+					   // de texto por lo que será tipo String
+	
+	myArray = new String[3];  // decimos cuantos elementos va a tener
+	
+	
+	
+	
 	}
-
 }
---
+++ misProyectos/src/misProyectos/otraPagina.java
@@ -0,0 +1,9 @@
+package misProyectos;
+
+public class otraPagina {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
