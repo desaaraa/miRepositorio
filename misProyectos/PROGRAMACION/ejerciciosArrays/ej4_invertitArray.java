246d54a832cfa246993f6dffda7658156df1fe72
USER <USER@DESKTOP-JC5RRLN>
2024-10-14 09:44:09 +0200

    ejs

--- misProyectos/PROGRAMACION/ejerciciosArrays/ej4_invertitArray.java
+++ misProyectos/PROGRAMACION/ejerciciosArrays/ej4_invertitArray.java
@@ -9,6 +9,20 @@ public class ej4_invertitArray {
 
 	public static void main(String[] args) {
 
+		int numeros[] = new int[5];
+		numeros[0] = 2;
+		numeros[1] = 5;
+		numeros[2] = 10;
+		numeros[3] = 20;
+		numeros[4] = 25;
+
+		int numeros2[] = new int[5];
+
+		for (int i = 0; i < 5; i++) {
+
+			numeros2[i] = numeros[4];
+		}
+
 	}
 
 }
