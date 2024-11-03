f441dc76bf0e5868959cee6e823a407c5962272b
Sara <Sara@192.168.1.156>
2024-11-03 11:32:52 +0100

    ej

--- misProyectos/PROGRAMACION/ejercicios_excepciones/ej1_ExcepcionNumeroNegativo.java
+++ misProyectos/PROGRAMACION/ejercicios_excepciones/ej1_ExcepcionNumeroNegativo.java
@@ -1,5 +1,7 @@
 package ejercicios_excepciones;
 
+import java.util.Scanner;
+
 /*
  * Crea un programa que pida al usuario que ingrese un
  *  número positivo. Si el usuario ingresa un número negativo
@@ -11,6 +13,23 @@ public class ej1_ExcepcionNumeroNegativo {
 
 	public static void main(String[] args) {
 
+		Scanner scanner = new Scanner(System.in);
+
+		try {
+			System.out.print("Por favor, ingrese un número positivo: ");
+
+			int numero = scanner.nextInt();
+
+			if (numero < 0) {
+				throw new IllegalArgumentException("El número no puede ser negativo.");
+			} else
+				System.out.println("Ha ingresado el número: " + numero);
+
+		} catch (java.util.InputMismatchException e) {
+			System.out.println("Debe introducir un numero, no caracteres");
+		}
+
+		scanner.close();
 	}
 
 }
